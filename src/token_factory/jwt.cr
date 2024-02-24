8de6a46c773c433a42955dd8b51da9c1f38aa0c5
George Dietrich <dev@dietrich.pub>
2024-02-24 14:39:40 -0500

    More provider types Spec coverage

--- src/token_factory/jwt.cr
+++ src/token_factory/jwt.cr
@@ -1,11 +1,42 @@
 struct Athena::Mercure::TokenFactory::JWT
   include Athena::Mercure::TokenFactory::Interface
 
+  # These make it easier to build the JSON in a type safe way vs dealing with merging hashes and such :shrug:
+  private record MercurePayload, subscribe : Array(String)?, publish : Array(String)? do
+    def to_json(builder : JSON::Builder) : Nil
+      builder.object do
+        if publish = @publish
+          builder.field "publish", publish
+        end
+
+        if subscribe = @subscribe
+          builder.field "subscribe", subscribe
+        end
+      end
+    end
+  end
+
+  private record Payload(T), mercure : MercurePayload, jwt_lifetime : Time::Span?, additional_claims : T do
+    def to_json(builder : JSON::Builder) : Nil
+      builder.object do
+        builder.field "mercure", @mercure
+
+        if (lifetime = @jwt_lifetime) && !@additional_claims.try &.has_key? "exp"
+          builder.field "exp", lifetime.total_seconds
+        end
+
+        additional_claims.try &.each do |k, v|
+          builder.field k, v
+        end
+      end
+    end
+  end
+
   @jwt_lifetime : Time::Span?
 
   def initialize(
     @jwt_secret : String,
-    @algorithm : JWT::Algorithm = :hs256,
+    @algorithm : ::JWT::Algorithm = :hs256,
     jwt_lifetime : Int32 | Time::Span | Nil = 3600,
     @passphrase : String = ""
   )
@@ -15,27 +46,12 @@ struct Athena::Mercure::TokenFactory::JWT
   def create(
     subscribe : Array(String)? = [] of String,
     publish : Array(String)? = [] of String,
-    additional_claims : Hash = {} of NoReturn => NoReturn
+    additional_claims : Hash? = nil
   ) : String
-    if !@jwt_lifetime.nil? && !additional_claims.has_key? "exp"
-      additional_claims["exp"] = @jwt_lifetime.total_seconds
-    end
-
-    tokens = {} of String => String | Array(String)
-    unless subscribe.empty?
-      tokens["subscribe"] = subscribe
-    end
-
-    unless publish.empty?
-      tokens["publish"] = publish
-    end
-
-    additional_claims["mercure"] = tokens
-
-    if mercure_claims = additional_claims["mercure"]
-      additional_claims["mercure"].merge! mercure_claims
-    end
-
-    JWT.encode additional_claims, @jwt_secret, @algorithm
+    ::JWT.encode(
+      Payload.new(MercurePayload.new(subscribe, publish), @jwt_lifetime, additional_claims),
+      @jwt_secret,
+      @algorithm
+    )
   end
 end
