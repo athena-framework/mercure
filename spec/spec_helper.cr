ef7c24b28ada83f8e601ccf97ae1cbd8c5913cc0
George Dietrich <dev@dietrich.pub>
2024-03-03 20:54:09 -0500

    Initial Implementation (#1)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,5 +3,6 @@ require "spec"
 require "athena-spec"
 
 require "../src/athena-mercure"
+require "../src/spec"
 
 ASPEC.run_all
--- src/athena-mercure.cr
+++ src/athena-mercure.cr
@@ -1,3 +1,17 @@
+require "jwt"
+
+require "http/client"
+require "http/headers"
+
+require "./authorization"
+require "./discovery"
+require "./update"
+
+require "./exceptions/*"
+require "./hub/*"
+require "./token_provider/*"
+require "./token_factory/*"
+
 # Convenience alias to make referencing `Athena::Mercure` types easier.
 alias AMC = Athena::Mercure
 
